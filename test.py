f41c658e71998511fdd5ed477a193ee51872d248
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-08-14 14:04:07 +0300

    add test module and no number dependence on grou names. closes #55

---
+++ test.py
@@ -0,0 +1,15 @@
+"""
+This will loop through all the files with group*.py and assert that their
+tweet()-function returns a string that is less than 140 characters long.
+
+
+"""
+import os
+
+for filename in os.listdir("."):
+    if filename.startswith("group_") and filename.endswith(".py"):
+        module_name = filename[:-2] # ignore .py at the end
+        module = __import__(module_name)
+        tweet_content = module.tweet().encode('utf-8')
+        assert len(tweet_content) <= 140, \
+            "tweet is too long: '{}'".format(tweet_content)
