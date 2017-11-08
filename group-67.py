eef94b319d7cd0eabcffc3b47d9abcc446b3eb41
Isak Johansson-Åkhe <isak.johansson-akhe@liu.se>
2017-11-08 10:09:08 +0100

    added tweet file for group 67

---
+++ group-67.py
@@ -0,0 +1,2 @@
+def tweet():
+    return "Once I used to know a gnu, that had a nose for news. A nose for news no newsman ever knew. But though he's dead I also knew another nosy gnu. No newsman ever knew the news that that gnu knew."
