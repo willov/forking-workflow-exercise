fa0bd198631e24ca6bb0eb6a42fb1dfccb10fbba
Isak Johansson-Åkhe <isak.johansson-akhe@liu.se>
2017-11-08 10:16:03 +0100

    fixed the too long tweet of 67

--- group-67.py
+++ group-67.py
@@ -1,2 +1,2 @@
 def tweet():
-    return "Once I used to know a gnu, that had a nose for news. A nose for news no newsman ever knew. But though he's dead I also knew another nosy gnu. No newsman ever knew the news that that gnu knew."
+    return "Once I used to make too long tweets, but I'm better now."
